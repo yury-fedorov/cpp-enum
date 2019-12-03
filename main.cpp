1aeaa417097bbe95e409f7d6ea1bcb88356d5ec5
FEDOROV Yury <fedoy@chzrhmac030.local>
2019-12-03 04:19:49 +0100

    simple enum, mapping

--- main.cpp
+++ main.cpp
@@ -8,23 +8,43 @@ enum Animal {
 	Dog, Cat, Pig
 };
 
-template <typename E, typename T>
-class EnumT {
+// only GetValues() as an addition
+template <typename E>
+class SimpleEnum {
 	E _value;
 public:
-	EnumT( const E & value ) : _value(value) { }
+	SimpleEnum( const E & value ) : _value(value) { }
 	virtual std::set<E> GetValues() const = 0;
-	virtual T toT( const E & value ) const = 0;
-	void set( const E & value ) { _value = value; }
-	void set( const T & value ) { _value = value; }
+
+    E Get() const { return _value; }
+	void Set( const E & value ) { _value = value; }
+};
+
+// mapping to an alternative type
+template <typename E, typename T>
+class EnumT : public SimpleEnum<E> {
+public:
+	EnumT( const E & value ) : SimpleEnum<E>(value) { }
+	virtual T To( const E & value ) const = 0;
+	// TODO - void Set( const T & value ) { _value = value; }
+
+    T To() const { return To( this->Get() ); }
 };
 
 class AnimalT : public EnumT<Animal,string> {
 public:
 	AnimalT( const Animal & value ) : EnumT(value) { }
 	std::set<Animal> GetValues() const { return std::set<Animal>(); }
-	string toT( const Animal & value ) const { return ""; };
+	string To( const Animal & value ) const { return ""; };
 };
+
+/*
+template <typename E>
+class EnumT1 : public EnumT<E,string> {
+protected:
+    typedef std::set<
+};
+*/
 
 int main() {
     string v("");
