723f1450435caf0992c17a7b54dfa99f21cfeb92
FEDOROV Yury <fedoy@chzrhmac030.local>
2019-12-03 05:55:20 +0100

    with auto

--- main.cpp
+++ main.cpp
@@ -44,15 +44,30 @@ private:
 protected:
     EnumStr( const E & value, GetValuesFunc map ) : AbstractEnumT<E,string>(value), _map(map) { }
 public:
-    std::set<E> GetValues() const { return set<E>(); }
-    string ToT( const E & value ) const { return ""; }
+    std::set<E> GetValues() const {
+        set<E> s;
+        set<ES> sp = _map();
+        auto i = sp.begin(); // TODO - fix for VS 2008 - no auto
+
+        for ( ; i != sp.end(); i++) s.insert(i->first);
+        return s;
+    }
+
+    string ToT( const E & value ) const {
+        set<ES> s = _map();
+        auto i = s.begin(); // TODO - fix for VS 2008 - no auto
+
+        for ( ; i != s.end(); i++)
+            if ( i->first == value )
+                return i->second;
+        return "";
+    }
 };
 
 typedef pair<Animal,string> AS;
+#define PAIR(type,name) pair<type,string>(name,#name)
 
-#define PAIR(type,name) type(name,#name)
-
-AS pairsAnimal [] = { PAIR(AS,Dog), PAIR(AS,Cat) };
+AS pairsAnimal [] = { PAIR(Animal,Dog), PAIR(Animal,Cat) };
 std::set<AS> setAnimal = set<AS>(begin(pairsAnimal), end(pairsAnimal));
 
 set<AS> SetAnimal() { return setAnimal; }
@@ -65,4 +80,8 @@ public:
 int main() {
     AnimalEnum a(Dog);
     cout << "Ciao " << a.To() <<  endl;
+
+    auto vs = a.GetValues();
+    for(auto i = vs.begin(); i != vs.end(); i++ )
+        cout << *i << " -> " << a.ToT(*i) << endl;
 }
